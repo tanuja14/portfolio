4ab4b046ca624e0500649af69003ea6d9686f05f
Tanuja Bhatnagar <tanuja.20489@ggnindia.dronacharya.info>
2024-05-25 13:27:02 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-const GET_USER_ARTICLES = `
+const GET_USER_ARTICLES = `query Publication(
+  $id: ObjectId="631db6be3e8d6f3497ad5d20"
+) {
+  publication(
+    id: $id
+  ) {
+    posts(first:5) {
+      edges {
+        node {
+          title,
+          brief,
+          slug,
+          url
+        }
+      }
+      totalDocuments
+    }
+  } 
+}`;
+/*`
 query Publication(
     $host: String="tanujabhatnagar.hashnode.dev"
   ) {
@@ -35,7 +54,8 @@ query Publication(
       }
     } 
   }
-`;
+`;*/
+
 gql(GET_USER_ARTICLES, { page: 0 })
     .then(result => {
         const articles = result.data.publication.posts.edges;
@@ -52,7 +72,6 @@ gql(GET_USER_ARTICLES, { page: 0 })
             let link = document.createElement('a');
             link.innerText = 'Read more...';
             link.href = `https://tanujabhatnagar.hashnode.dev/${article.node.slug}`;
-
             container.appendChild(title);
             container.appendChild(brief);
             container.appendChild(link);
